c5a3b0e74be81512d4866bef4aa2d666b9238d95
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-09-17 21:49:21 +0800

    feat: Add outbound provider support
    Start DNS transports before provider
    Add kTLS support
    Add AnyTLS link parser
    Add tls-pin-sha256 support
    Add `override_dialer` option
    Support override TLS option
    Add `path` option for remote provider
    Fix clash `tls` parser
    Fix slow startup
    
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>
    Co-authored-by: yelnoo <t50500411@163.com>
    Co-authored-by: reF1nd <ref1nd@thunder-cloud.cc>
    Co-authored-by: dyhkwong <50692134+dyhkwong@users.noreply.github.com>
    Co-authored-by: lux5am <lux.5am@yandex.com>
    Co-authored-by: xireiki <i@xireiki.com>

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"time"
 
+	"github.com/sagernet/sing-box/common/hash"
 	"github.com/sagernet/sing/common/observable"
 	"github.com/sagernet/sing/common/varbin"
 )
@@ -55,9 +56,12 @@ type CacheFile interface {
 	StoreGroupExpand(group string, expand bool) error
 	LoadRuleSet(tag string) *SavedBinary
 	SaveRuleSet(tag string, set *SavedBinary) error
+	LoadSubscription(tag string) *SavedBinary
+	SaveSubscription(tag string, sub *SavedBinary) error
 }
 
 type SavedBinary struct {
+	Hash        hash.HashType
 	Content     []byte
 	LastUpdated time.Time
 	LastEtag    string
@@ -66,6 +70,18 @@ type SavedBinary struct {
 func (s *SavedBinary) MarshalBinary() ([]byte, error) {
 	var buffer bytes.Buffer
 	err := binary.Write(&buffer, binary.BigEndian, uint8(1))
+	if err != nil {
+		return nil, err
+	}
+	hash, err := s.Hash.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	_, err = varbin.WriteUvarint(&buffer, uint64(len(hash)))
+	if err != nil {
+		return nil, err
+	}
+	_, err = buffer.Write(hash)
 	if err != nil {
 		return nil, err
 	}
@@ -96,6 +112,19 @@ func (s *SavedBinary) UnmarshalBinary(data []byte) error {
 	reader := bytes.NewReader(data)
 	var version uint8
 	err := binary.Read(reader, binary.BigEndian, &version)
+	if err != nil {
+		return err
+	}
+	hashLength, err := binary.ReadUvarint(reader)
+	if err != nil {
+		return err
+	}
+	hash := make([]byte, hashLength)
+	_, err = io.ReadFull(reader, hash)
+	if err != nil {
+		return err
+	}
+	err = s.Hash.UnmarshalBinary(hash)
 	if err != nil {
 		return err
 	}
