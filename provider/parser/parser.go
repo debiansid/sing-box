6defc80192e35378864cd1338af54aff4d2f1909
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-09-17 21:49:21 +0800

    feat: Add outbound provider support
    Start DNS transports before provider
    Add kTLS support
    Add AnyTLS link parser
    Add tls-pin-sha256 support
    Add `override_dialer` option
    Support override TLS option
    Add `path` option for remote provider
    Fix clash `tls` parser
    Fix slow startup
    
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>
    Co-authored-by: yelnoo <t50500411@163.com>
    Co-authored-by: reF1nd <ref1nd@thunder-cloud.cc>
    Co-authored-by: dyhkwong <50692134+dyhkwong@users.noreply.github.com>
    Co-authored-by: lux5am <lux.5am@yandex.com>
    Co-authored-by: xireiki <i@xireiki.com>

---
+++ provider/parser/parser.go
@@ -0,0 +1,193 @@
+package parser
+
+import (
+	"context"
+	"reflect"
+
+	C "github.com/sagernet/sing-box/constant"
+	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common"
+	E "github.com/sagernet/sing/common/exceptions"
+)
+
+var subscriptionParsers = []func(ctx context.Context, content string) ([]option.Outbound, error){
+	ParseBoxSubscription,
+	ParseClashSubscription,
+	ParseSIP008Subscription,
+	ParseRawSubscription,
+}
+
+func ParseSubscription(ctx context.Context, content string, overrideDialerOptions *option.OverrideDialerOptions, overrideTLSOptions *option.OverrideTLSOptions) ([]option.Outbound, error) {
+	var pErr error
+	for _, parser := range subscriptionParsers {
+		servers, err := parser(ctx, content)
+		if len(servers) > 0 {
+			return overrideOutbounds(servers, overrideDialerOptions, overrideTLSOptions), nil
+		}
+		pErr = E.Errors(pErr, err)
+	}
+	return nil, E.Cause(pErr, "no servers found")
+}
+
+func overrideOutbounds(outbounds []option.Outbound, overrideDialerOptions *option.OverrideDialerOptions, overrideTLSOptions *option.OverrideTLSOptions) []option.Outbound {
+	var tags []string
+	for _, outbound := range outbounds {
+		tags = append(tags, outbound.Tag)
+	}
+	var parsedOutbounds []option.Outbound
+	for _, outbound := range outbounds {
+		switch outbound.Type {
+		case C.TypeHTTP:
+			options := outbound.Options.(*option.HTTPOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeSOCKS:
+			options := outbound.Options.(*option.SOCKSOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			outbound.Options = options
+		case C.TypeTUIC:
+			options := outbound.Options.(*option.TUICOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeVMess:
+			options := outbound.Options.(*option.VMessOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeVLESS:
+			options := outbound.Options.(*option.VLESSOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeTrojan:
+			options := outbound.Options.(*option.TrojanOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeHysteria:
+			options := outbound.Options.(*option.HysteriaOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeShadowTLS:
+			options := outbound.Options.(*option.ShadowTLSOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeHysteria2:
+			options := outbound.Options.(*option.Hysteria2OutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeAnyTLS:
+			options := outbound.Options.(*option.AnyTLSOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			options.OutboundTLSOptionsContainer.TLS = overrideTLSOption(options.OutboundTLSOptionsContainer.TLS, overrideTLSOptions)
+			outbound.Options = options
+		case C.TypeShadowsocks:
+			options := outbound.Options.(*option.ShadowsocksOutboundOptions)
+			options.DialerOptions = overrideDialerOption(options.DialerOptions, overrideDialerOptions, tags)
+			outbound.Options = options
+		}
+		parsedOutbounds = append(parsedOutbounds, outbound)
+	}
+	return parsedOutbounds
+}
+
+func overrideDialerOption(options option.DialerOptions, overrideDialerOptions *option.OverrideDialerOptions, tags []string) option.DialerOptions {
+	if options.Detour != "" && !common.Any(tags, func(tag string) bool {
+		return options.Detour == tag
+	}) {
+		options.Detour = ""
+	}
+	var defaultOptions option.OverrideDialerOptions
+	if overrideDialerOptions == nil || reflect.DeepEqual(*overrideDialerOptions, defaultOptions) {
+		return options
+	}
+	if overrideDialerOptions.Detour != nil && options.Detour == "" {
+		options.Detour = *overrideDialerOptions.Detour
+	}
+	if overrideDialerOptions.BindInterface != nil {
+		options.BindInterface = *overrideDialerOptions.BindInterface
+	}
+	if overrideDialerOptions.Inet4BindAddress != nil {
+		options.Inet4BindAddress = overrideDialerOptions.Inet4BindAddress
+	}
+	if overrideDialerOptions.Inet6BindAddress != nil {
+		options.Inet6BindAddress = overrideDialerOptions.Inet6BindAddress
+	}
+	if overrideDialerOptions.ProtectPath != nil {
+		options.ProtectPath = *overrideDialerOptions.ProtectPath
+	}
+	if overrideDialerOptions.RoutingMark != nil {
+		options.RoutingMark = *overrideDialerOptions.RoutingMark
+	}
+	if overrideDialerOptions.ReuseAddr != nil {
+		options.ReuseAddr = *overrideDialerOptions.ReuseAddr
+	}
+	if overrideDialerOptions.ConnectTimeout != nil {
+		options.ConnectTimeout = *overrideDialerOptions.ConnectTimeout
+	}
+	if overrideDialerOptions.TCPFastOpen != nil {
+		options.TCPFastOpen = *overrideDialerOptions.TCPFastOpen
+	}
+	if overrideDialerOptions.TCPMultiPath != nil {
+		options.TCPMultiPath = *overrideDialerOptions.TCPMultiPath
+	}
+	if overrideDialerOptions.UDPFragment != nil {
+		options.UDPFragment = overrideDialerOptions.UDPFragment
+	}
+	if overrideDialerOptions.DomainResolver != nil {
+		options.DomainResolver = overrideDialerOptions.DomainResolver
+	}
+	if overrideDialerOptions.NetworkStrategy != nil {
+		options.NetworkStrategy = overrideDialerOptions.NetworkStrategy
+	}
+	if overrideDialerOptions.NetworkType != nil {
+		options.NetworkType = *overrideDialerOptions.NetworkType
+	}
+	if overrideDialerOptions.FallbackNetworkType != nil {
+		options.FallbackNetworkType = *overrideDialerOptions.FallbackNetworkType
+	}
+	if overrideDialerOptions.FallbackDelay != nil {
+		options.FallbackDelay = *overrideDialerOptions.FallbackDelay
+	}
+	if overrideDialerOptions.DomainStrategy != nil {
+		options.DomainStrategy = *overrideDialerOptions.DomainStrategy
+	}
+	return options
+}
+
+func overrideTLSOption(options *option.OutboundTLSOptions, overrideTLSOptions *option.OverrideTLSOptions) *option.OutboundTLSOptions {
+	if options == nil {
+		return options
+	}
+	var defaultOptions option.OutboundTLSOptions
+	if overrideTLSOptions == nil || reflect.DeepEqual(*overrideTLSOptions, defaultOptions) {
+		return options
+	}
+	if overrideTLSOptions.Enabled != nil && !*overrideTLSOptions.Enabled {
+		return &defaultOptions
+	}
+	// if override.OverrideTLSOptions.Enabled != nil {
+	// options.Enabled = *override.OverrideTLSOptions.Enabled
+	// }
+	if overrideTLSOptions.DisableSNI != nil {
+		options.DisableSNI = *overrideTLSOptions.DisableSNI
+	}
+	if overrideTLSOptions.ServerName != nil {
+		options.ServerName = *overrideTLSOptions.ServerName
+	}
+	if overrideTLSOptions.Insecure != nil {
+		options.Insecure = *overrideTLSOptions.Insecure
+	}
+	if overrideTLSOptions.KernelTx != nil {
+		options.KernelTx = *overrideTLSOptions.KernelTx
+	}
+	if overrideTLSOptions.KernelRx != nil {
+		options.KernelRx = *overrideTLSOptions.KernelRx
+	}
+	return options
+}
---
+++ provider/remote/remote.go
@@ -0,0 +1,467 @@
+package remote
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/adapter/provider"
+	"github.com/sagernet/sing-box/common/hash"
+	"github.com/sagernet/sing-box/common/interrupt"
+	C "github.com/sagernet/sing-box/constant"
+	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing-box/provider/parser"
+	"github.com/sagernet/sing/common"
+	E "github.com/sagernet/sing/common/exceptions"
+	F "github.com/sagernet/sing/common/format"
+	"github.com/sagernet/sing/common/json"
+	M "github.com/sagernet/sing/common/metadata"
+	N "github.com/sagernet/sing/common/network"
+	"github.com/sagernet/sing/common/ntp"
+	"github.com/sagernet/sing/common/rw"
+	"github.com/sagernet/sing/service"
+	"github.com/sagernet/sing/service/filemanager"
+)
+
+func RegisterProvider(registry *provider.Registry) {
+	provider.Register[option.ProviderRemoteOptions](registry, C.ProviderTypeRemote, NewProviderRemote)
+}
+
+var _ adapter.Provider = (*ProviderRemote)(nil)
+
+type ProviderRemote struct {
+	provider.Adapter
+	ctx              context.Context
+	cancel           context.CancelFunc
+	logger           log.ContextLogger
+	outbound         adapter.OutboundManager
+	provider         adapter.ProviderManager
+	cacheFile        adapter.CacheFile
+	dialer           N.Dialer
+	hash             hash.HashType
+	lastEtag         string
+	lastOutOpts      []option.Outbound
+	lastUpdated      time.Time
+	subscriptionInfo adapter.SubscriptionInfo
+	ticker           *time.Ticker
+	updating         atomic.Bool
+
+	url            string
+	path           string
+	userAgent      string
+	downloadDetour string
+	updateInterval time.Duration
+	exclude        *regexp.Regexp
+	include        *regexp.Regexp
+
+	overrideDialer *option.OverrideDialerOptions
+	overrideTLS    *option.OverrideTLSOptions
+}
+
+func NewProviderRemote(ctx context.Context, router adapter.Router, logFactory log.Factory, tag string, options option.ProviderRemoteOptions) (adapter.Provider, error) {
+	if options.URL == "" {
+		return nil, E.New("provider URL is required")
+	}
+	var path string
+	if options.Path != "" {
+		path = filemanager.BasePath(ctx, options.Path)
+		path, _ = filepath.Abs(path)
+	}
+	if rw.IsDir(path) {
+		return nil, E.New("provider path is a directory: ", path)
+	}
+	updateInterval := time.Duration(options.UpdateInterval)
+	if updateInterval <= 0 {
+		updateInterval = 24 * time.Hour
+	}
+	if updateInterval < time.Hour {
+		updateInterval = time.Hour
+	}
+	var userAgent string
+	if options.UserAgent == "" {
+		userAgent = "sing-box " + C.Version
+	} else {
+		userAgent = options.UserAgent
+	}
+	ctx, cancel := context.WithCancel(ctx)
+	outbound := service.FromContext[adapter.OutboundManager](ctx)
+	logger := logFactory.NewLogger(F.ToString("provider/remote", "[", tag, "]"))
+	updateChan := make(chan struct{})
+	close(updateChan)
+	return &ProviderRemote{
+		Adapter:  provider.NewAdapter(ctx, router, outbound, logFactory, logger, tag, C.ProviderTypeRemote, options.HealthCheck),
+		ctx:      ctx,
+		cancel:   cancel,
+		logger:   logger,
+		outbound: outbound,
+		provider: service.FromContext[adapter.ProviderManager](ctx),
+
+		url:            options.URL,
+		path:           path,
+		userAgent:      userAgent,
+		downloadDetour: options.DownloadDetour,
+		updateInterval: updateInterval,
+		exclude:        (*regexp.Regexp)(options.Exclude),
+		include:        (*regexp.Regexp)(options.Include),
+
+		overrideDialer: options.OverrideDialer,
+		overrideTLS:    options.OverrideTLS,
+	}, nil
+}
+
+func (s *ProviderRemote) StartContext(ctx context.Context, startContext *adapter.HTTPStartContext) error {
+	s.cacheFile = service.FromContext[adapter.CacheFile](s.ctx)
+	err := s.loadCacheFile()
+	if err != nil {
+		return E.Cause(err, "restore cached outbound provider")
+	}
+	if s.downloadDetour != "" {
+		outbound, loaded := s.outbound.Outbound(s.downloadDetour)
+		if !loaded {
+			return E.New("detour outbound not found: ", s.downloadDetour)
+		}
+		s.dialer = outbound
+	} else {
+		s.dialer = s.outbound.Default()
+	}
+	if s.lastUpdated.IsZero() {
+		ctx = interrupt.ContextWithIsProviderConnection(ctx)
+		err := s.fetch(ctx, startContext)
+		if err != nil {
+			return E.Cause(err, "initial outbound provider: ", s.Tag())
+		}
+	}
+	go s.loopUpdate()
+	return s.Adapter.Start()
+}
+
+func (s *ProviderRemote) Update() error {
+	if s.ticker != nil {
+		s.ticker.Reset(s.updateInterval)
+	}
+	ctx := interrupt.ContextWithIsProviderConnection(s.ctx)
+	return s.fetch(ctx, nil)
+}
+
+func (s *ProviderRemote) UpdatedAt() time.Time {
+	return s.lastUpdated
+}
+
+func (s *ProviderRemote) SubscriptionInfo() adapter.SubscriptionInfo {
+	return s.subscriptionInfo
+}
+
+func (s *ProviderRemote) Close() error {
+	s.cancel()
+	if s.ticker != nil {
+		s.ticker.Stop()
+	}
+	return common.Close(&s.Adapter)
+}
+
+func (s *ProviderRemote) updateOnce() {
+	ctx := interrupt.ContextWithIsProviderConnection(s.ctx)
+	if err := s.fetch(ctx, nil); err != nil {
+		s.logger.Error("update outbound provider: ", err)
+	}
+}
+
+func (s *ProviderRemote) fetch(ctx context.Context, startContext *adapter.HTTPStartContext) error {
+	if s.updating.Swap(true) {
+		return E.New("provider is updating")
+	}
+	defer s.updating.Store(false)
+	s.logger.Debug("updating outbound provider ", s.Tag(), " from URL: ", s.url)
+	var client *http.Client
+	if startContext != nil {
+		client = startContext.HTTPClient(s.downloadDetour, s.dialer)
+	} else {
+		client = &http.Client{
+			Transport: &http.Transport{
+				ForceAttemptHTTP2:   true,
+				TLSHandshakeTimeout: C.TCPTimeout,
+				DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+					return s.dialer.DialContext(ctx, network, M.ParseSocksaddr(addr))
+				},
+				TLSClientConfig: &tls.Config{
+					Time:    ntp.TimeFuncFromContext(ctx),
+					RootCAs: adapter.RootPoolFromContext(ctx),
+				},
+			},
+		}
+	}
+	req, err := http.NewRequest(http.MethodGet, s.url, nil)
+	if err != nil {
+		return err
+	}
+	if s.lastEtag != "" {
+		req.Header.Set("If-None-Match", s.lastEtag)
+	}
+	req.Header.Set("User-Agent", s.userAgent)
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+	infoStr := resp.Header.Get("subscription-userinfo")
+	info, hasInfo := parseInfo(infoStr)
+	switch resp.StatusCode {
+	case http.StatusOK:
+	case http.StatusNotModified:
+		s.subscriptionInfo = info
+		s.lastUpdated = time.Now()
+		if s.cacheFile != nil {
+			saveSub := s.cacheFile.LoadSubscription(s.Tag())
+			if saveSub != nil {
+				if s.path != "" {
+					saveSub.Hash = s.hash
+				} else if hasInfo {
+					index := bytes.IndexByte(saveSub.Content, '\n')
+					if index != -1 {
+						saveSub.Content = append([]byte(infoStr+"\n"), saveSub.Content[index+1:]...)
+					}
+				}
+				saveSub.LastUpdated = s.lastUpdated
+				if err := s.cacheFile.SaveSubscription(s.Tag(), saveSub); err != nil {
+					s.logger.Error("save outbound provider cache file: ", err)
+				}
+			}
+		}
+		if s.path != "" {
+			content, _ := json.Marshal(option.Options{
+				Outbounds: s.lastOutOpts,
+			})
+			s.saveCacheFile(hasInfo, info, content)
+		}
+		s.logger.Info("update outbound provider ", s.Tag(), ": not modified")
+		return nil
+	default:
+		return E.New("unexpected status: ", resp.Status)
+	}
+	defer resp.Body.Close()
+	contentRaw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	eTagHeader := resp.Header.Get("Etag")
+	if eTagHeader != "" {
+		s.lastEtag = eTagHeader
+	}
+	content, _ := parser.DecodeBase64URLSafe(string(contentRaw))
+	if !hasInfo {
+		firstLine, others := getFirstLine(content)
+		if info, hasInfo = parseInfo(firstLine); hasInfo {
+			infoStr = firstLine
+			content, _ = parser.DecodeBase64URLSafe(others)
+		}
+	}
+	if err := s.updateProviderFromContent(content); err != nil {
+		return err
+	}
+	s.UpdateGroups()
+	s.subscriptionInfo = info
+	s.lastUpdated = time.Now()
+	if s.path != "" || s.cacheFile != nil {
+		content, _ := json.Marshal(option.Options{
+			Outbounds: s.lastOutOpts,
+		})
+		if s.path != "" {
+			s.saveCacheFile(hasInfo, info, content)
+		} else if hasInfo {
+			content = append([]byte(infoStr+"\n"), content...)
+		}
+		if s.cacheFile != nil {
+			saveSub := &adapter.SavedBinary{
+				LastUpdated: s.lastUpdated,
+				LastEtag:    s.lastEtag,
+			}
+			if s.path != "" {
+				saveSub.Hash = s.hash
+			} else {
+				saveSub.Content = content
+			}
+			if err = s.cacheFile.SaveSubscription(s.Tag(), saveSub); err != nil {
+				s.logger.Error("save outbound provider cache file: ", err)
+			}
+		}
+	}
+	s.logger.Info("updated outbound provider ", s.Tag())
+	return nil
+}
+
+func (s *ProviderRemote) loadCacheFile() error {
+	var content []byte
+	var lastUpdated time.Time
+	var lastEtag string
+	var saveSub *adapter.SavedBinary
+	if s.cacheFile != nil {
+		if saveSub = s.cacheFile.LoadSubscription(s.Tag()); saveSub != nil {
+			s.hash = saveSub.Hash
+		}
+	}
+	if s.path != "" {
+		exists, err := pathExists(s.path)
+		if err != nil {
+			return err
+		}
+		if !exists {
+			return nil
+		}
+		file, _ := os.Open(s.path)
+		content, err = io.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		if saveSub != nil {
+			if !s.hash.Equal(hash.MakeHash(content)) {
+				s.logger.Error("load outbound provider cache file failed: validation failed")
+				return nil
+			}
+			lastUpdated = saveSub.LastUpdated
+			lastEtag = saveSub.LastEtag
+		} else {
+			fs, _ := file.Stat()
+			lastUpdated = fs.ModTime()
+		}
+	} else if saveSub != nil && saveSub.Content != nil {
+		content = saveSub.Content
+		lastUpdated = saveSub.LastUpdated
+		lastEtag = saveSub.LastEtag
+	} else {
+		return nil
+	}
+	if err := s.loadFromContent(content); err != nil {
+		return err
+	}
+	s.UpdateGroups()
+	s.lastUpdated, s.lastEtag = lastUpdated, lastEtag
+	return nil
+}
+
+func (s *ProviderRemote) loadFromContent(contentRaw []byte) error {
+	content, _ := parser.DecodeBase64URLSafe(string(contentRaw))
+	firstLine, others := getFirstLine(content)
+	if info, ok := parseInfo(firstLine); ok {
+		s.subscriptionInfo = info
+		content, _ = parser.DecodeBase64URLSafe(others)
+	}
+	if err := s.updateProviderFromContent(content); err != nil {
+		return err
+	}
+	return nil
+}
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func (s *ProviderRemote) loopUpdate() {
+	if time.Since(s.lastUpdated) < s.updateInterval {
+		select {
+		case <-s.ctx.Done():
+			return
+		case <-time.After(time.Until(s.lastUpdated.Add(s.updateInterval))):
+			s.updateOnce()
+		}
+	} else {
+		s.updateOnce()
+	}
+	s.ticker = time.NewTicker(s.updateInterval)
+	for {
+		runtime.GC()
+		select {
+		case <-s.ctx.Done():
+			return
+		case <-s.ticker.C:
+			s.updateOnce()
+		}
+	}
+}
+
+func (s *ProviderRemote) saveCacheFile(hasInfo bool, info adapter.SubscriptionInfo, contentRaw []byte) {
+	content := contentRaw
+	if hasInfo {
+		infoStr := fmt.Sprint(
+			"# upload=", info.Upload,
+			"; download=", info.Download,
+			"; total=", info.Total,
+			"; expire=", info.Expire,
+			";")
+		content = append([]byte(infoStr+"\n"), content...)
+	}
+	s.hash = hash.MakeHash(content)
+	dir := filepath.Dir(s.path)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		filemanager.MkdirAll(s.ctx, dir, 0o755)
+	}
+	filemanager.WriteFile(s.ctx, s.path, []byte(content), 0o666)
+}
+
+func (s *ProviderRemote) updateProviderFromContent(content string) error {
+	outboundOpts, err := parser.ParseSubscription(s.ctx, content, s.overrideDialer, s.overrideTLS)
+	if err != nil {
+		return err
+	}
+	outboundOpts = common.Filter(outboundOpts, func(it option.Outbound) bool {
+		return (s.exclude == nil || !s.exclude.MatchString(it.Tag)) && (s.include == nil || s.include.MatchString(it.Tag))
+	})
+	s.UpdateOutbounds(s.lastOutOpts, outboundOpts)
+	s.lastOutOpts = outboundOpts
+	return nil
+}
+
+func getFirstLine(content string) (string, string) {
+	lines := strings.Split(content, "\n")
+	if len(lines) == 1 {
+		return lines[0], ""
+	}
+	others := strings.Join(lines[1:], "\n")
+	return lines[0], others
+}
+
+func parseInfo(infoStr string) (adapter.SubscriptionInfo, bool) {
+	info := adapter.SubscriptionInfo{}
+	if infoStr == "" {
+		return info, false
+	}
+	reg := regexp.MustCompile(`(upload|download|total|expire)[\s\t]*=[\s\t]*(-?\d*);?`)
+	matches := reg.FindAllStringSubmatch(infoStr, 4)
+	if len(matches) == 0 {
+		return info, false
+	}
+	for _, match := range matches {
+		key, value := match[1], match[2]
+		switch key {
+		case "upload":
+			info.Upload = parser.StringToType[int64](value)
+		case "download":
+			info.Download = parser.StringToType[int64](value)
+		case "total":
+			info.Total = parser.StringToType[int64](value)
+		case "expire":
+			info.Expire = parser.StringToType[int64](value)
+		default:
+			return info, false
+		}
+	}
+	return info, true
+}
