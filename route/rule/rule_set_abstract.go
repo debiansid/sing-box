fd50fa14c932951e00fe7bcb22a1a4336eb3c9a4
PuerNya <safarier@outlook.com>
2024-08-13 05:56:16 +0800

    clash-api: Add rule provider support
    use static file instead of cache file
    makes remote rule-set `path` correct

---
+++ route/rule/rule_set_abstract.go
@@ -0,0 +1,116 @@
+package rule
+
+import (
+	"context"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common"
+	E "github.com/sagernet/sing/common/exceptions"
+	F "github.com/sagernet/sing/common/format"
+	"github.com/sagernet/sing/common/logger"
+
+	"go4.org/netipx"
+)
+
+type abstractRuleSet struct {
+	ctx         context.Context
+	logger      logger.ContextLogger
+	tag         string
+	access      sync.RWMutex
+	sType       string
+	path        string
+	format      string
+	rules       []adapter.HeadlessRule
+	ruleCount   uint64
+	metadata    adapter.RuleSetMetadata
+	lastUpdated time.Time
+	refs        atomic.Int32
+}
+
+func (s *abstractRuleSet) Name() string {
+	return s.tag
+}
+
+func (s *abstractRuleSet) Type() string {
+	return s.sType
+}
+
+func (s *abstractRuleSet) Format() string {
+	return s.format
+}
+
+func (s *abstractRuleSet) RuleCount() uint64 {
+	return s.ruleCount
+}
+
+func (s *abstractRuleSet) UpdatedTime() time.Time {
+	return s.lastUpdated
+}
+
+func (s *abstractRuleSet) String() string {
+	return strings.Join(F.MapToString(s.rules), " ")
+}
+
+func (s *abstractRuleSet) Metadata() adapter.RuleSetMetadata {
+	s.access.RLock()
+	defer s.access.RUnlock()
+	return s.metadata
+}
+
+func (s *abstractRuleSet) ExtractIPSet() []*netipx.IPSet {
+	s.access.RLock()
+	defer s.access.RUnlock()
+	return common.FlatMap(s.rules, extractIPSetFromRule)
+}
+
+func (s *abstractRuleSet) IncRef() {
+	s.refs.Add(1)
+}
+
+func (s *abstractRuleSet) DecRef() {
+	if s.refs.Add(-1) < 0 {
+		panic("rule-set: negative refs")
+	}
+}
+
+func (s *abstractRuleSet) Cleanup() {
+	if s.refs.Load() == 0 {
+		s.rules = nil
+	}
+}
+
+func (s *abstractRuleSet) reloadRules(headlessRules []option.HeadlessRule) error {
+	rules := make([]adapter.HeadlessRule, len(headlessRules))
+	var ruleCount uint64
+	for i, ruleOptions := range headlessRules {
+		rule, err := NewHeadlessRule(s.ctx, ruleOptions)
+		if err != nil {
+			return E.Cause(err, "parse rule_set.rules.[", i, "]")
+		}
+		rules[i] = rule
+		ruleCount += rule.RuleCount()
+	}
+	var metadata adapter.RuleSetMetadata
+	metadata.ContainsProcessRule = HasHeadlessRule(headlessRules, isProcessHeadlessRule)
+	metadata.ContainsWIFIRule = HasHeadlessRule(headlessRules, isWIFIHeadlessRule)
+	metadata.ContainsIPCIDRRule = HasHeadlessRule(headlessRules, isIPCIDRHeadlessRule)
+	s.access.Lock()
+	s.rules = rules
+	s.ruleCount = ruleCount
+	s.metadata = metadata
+	return nil
+}
+
+func (s *abstractRuleSet) Match(metadata *adapter.InboundContext) bool {
+	for _, rule := range s.rules {
+		if rule.Match(metadata) {
+			return true
+		}
+	}
+	return false
+}
