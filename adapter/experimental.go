edebb78c6b737532c93b43c338e37beffa124025
yelnoo <t50500411@163.com>
2025-03-12 07:56:09 +0800

    feat: Add outbound provider support
    Add kTLS support
    Add AnyTLS link parser
    Add tls-pin-sha256 support
    Add `path` option for remote provider
    Fix clash `tls` parser
    Fix slow startup
    
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>
    Co-authored-by: yelnoo <t50500411@163.com>
    Co-authored-by: reF1nd <ref1nd@thunder-cloud.cc>
    Co-authored-by: lux5am <lux.5am@yandex.com>

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"time"
 
+	"github.com/sagernet/sing-box/common/hash"
 	"github.com/sagernet/sing/common/observable"
 	"github.com/sagernet/sing/common/varbin"
 )
@@ -55,9 +56,12 @@ type CacheFile interface {
 	StoreGroupExpand(group string, expand bool) error
 	LoadRuleSet(tag string) *SavedBinary
 	SaveRuleSet(tag string, set *SavedBinary) error
+	LoadSubscription(tag string) *SavedBinary
+	SaveSubscription(tag string, sub *SavedBinary) error
 }
 
 type SavedBinary struct {
+	Hash        hash.HashType
 	Content     []byte
 	LastUpdated time.Time
 	LastEtag    string
@@ -69,11 +73,12 @@ func (s *SavedBinary) MarshalBinary() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	_, err = varbin.WriteUvarint(&buffer, uint64(len(s.Content)))
+	hash, _ := s.Hash.MarshalBinary()
+	err = binary.Write(&buffer, binary.BigEndian, hash)
 	if err != nil {
 		return nil, err
 	}
-	_, err = buffer.Write(s.Content)
+	err = varbin.Write(&buffer, binary.BigEndian, s.Content)
 	if err != nil {
 		return nil, err
 	}
@@ -99,12 +104,16 @@ func (s *SavedBinary) UnmarshalBinary(data []byte) error {
 	if err != nil {
 		return err
 	}
-	contentLength, err := binary.ReadUvarint(reader)
+	var hash [16]byte
+	err = binary.Read(reader, binary.BigEndian, &hash)
 	if err != nil {
 		return err
 	}
-	s.Content = make([]byte, contentLength)
-	_, err = io.ReadFull(reader, s.Content)
+	err = s.Hash.UnmarshalBinary(hash)
+	if err != nil {
+		return err
+	}
+	err = varbin.Read(reader, binary.BigEndian, &s.Content)
 	if err != nil {
 		return err
 	}
