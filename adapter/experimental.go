b1f758d38f9bed149b72d97b5a5306716b0b9bf7
yelnoo <t50500411@163.com>
2025-03-12 07:56:09 +0800

    add outbound provider
    Co-authored-by: jebbs <qjebbs@gmail.com>
    Co-authored-by: PuerNya <safarier@outlook.com>

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"time"
 
+	"github.com/sagernet/sing-box/common/hash"
 	E "github.com/sagernet/sing/common/exceptions"
 	"github.com/sagernet/sing/common/varbin"
 )
@@ -47,9 +48,13 @@ type CacheFile interface {
 	StoreGroupExpand(group string, expand bool) error
 	LoadRuleSet(tag string) *SavedBinary
 	SaveRuleSet(tag string, set *SavedBinary) error
+	LoadSubscription(tag string) *SavedBinary
+	SaveSubscription(tag string, sub *SavedBinary) error
 }
 
 type SavedBinary struct {
+	// Hash is stored only by the branch-private cache envelope.
+	Hash        hash.HashType
 	Content     []byte
 	LastUpdated time.Time
 	LastEtag    string
@@ -94,6 +99,7 @@ func (s *SavedBinary) MarshalBinary() ([]byte, error) {
 }
 
 func (s *SavedBinary) UnmarshalBinary(data []byte) error {
+	*s = SavedBinary{}
 	reader := bytes.NewReader(data)
 	var version uint8
 	err := binary.Read(reader, binary.BigEndian, &version)
