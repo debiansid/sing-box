f7458dc31da6df172631f13f26bc96a729fcb45e
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-03-18 14:28:16 +0800

    Only retry TCP sniffing for possible TLS Client Hello

--- common/sniff/sniff.go
+++ common/sniff/sniff.go
@@ -3,6 +3,7 @@ package sniff
 import (
 	"bytes"
 	"context"
+	goerrors "errors"
 	"io"
 	"net"
 	"time"
@@ -40,7 +41,7 @@ func PeekStream(ctx context.Context, metadata *adapter.InboundContext, conn net.
 	}
 	deadline := time.Now().Add(timeout)
 	var errors []error
-	for i := 0; ; i++ {
+	for {
 		err := conn.SetReadDeadline(deadline)
 		if err != nil {
 			return E.Cause(err, "set read deadline")
@@ -48,9 +49,6 @@ func PeekStream(ctx context.Context, metadata *adapter.InboundContext, conn net.
 		_, err = buffer.ReadOnceFrom(conn)
 		_ = conn.SetReadDeadline(time.Time{})
 		if err != nil {
-			if i > 0 {
-				break
-			}
 			return E.Cause(err, "read payload")
 		}
 		errors = nil
@@ -60,6 +58,9 @@ func PeekStream(ctx context.Context, metadata *adapter.InboundContext, conn net.
 				return nil
 			}
 			errors = append(errors, err)
+		}
+		if !goerrors.Is(E.Errors(errors...), errPossibleClientHello) {
+			break
 		}
 	}
 	return E.Errors(errors...)
