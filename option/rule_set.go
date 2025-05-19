fed8857de9193e7fcb113c20c93c50c33d9df773
千尘白仙 <47497429+HGSUWN@users.noreply.github.com>
2025-05-19 20:53:19 +0800

    Update rule_set.go
    Signed-off-by: 千尘白仙 <47497429+HGSUWN@users.noreply.github.com>

--- option/rule_set.go
+++ option/rule_set.go
@@ -23,6 +23,7 @@ type _RuleSet struct {
 	Format        string        `json:"format,omitempty"`
 	Path          string        `json:"path,omitempty"`
 	InlineOptions PlainRuleSet  `json:"-"`
+	LocalOptions  LocalRuleSet  `json:"-"`
 	RemoteOptions RemoteRuleSet `json:"-"`
 }
 
@@ -33,7 +34,7 @@ func (r RuleSet) MarshalJSON() ([]byte, error) {
 		var defaultFormat string
 		switch r.Type {
 		case C.RuleSetTypeLocal:
-			defaultFormat = ruleSetDefaultFormat(r.Path)
+			defaultFormat = ruleSetDefaultFormat(r.LocalOptions.Path)
 		case C.RuleSetTypeRemote:
 			defaultFormat = ruleSetDefaultFormat(r.RemoteOptions.URL)
 		}
@@ -47,7 +48,7 @@ func (r RuleSet) MarshalJSON() ([]byte, error) {
 		r.Type = ""
 		v = r.InlineOptions
 	case C.RuleSetTypeLocal:
-		v = nil
+		v = r.LocalOptions
 	case C.RuleSetTypeRemote:
 		v = r.RemoteOptions
 	default:
@@ -70,7 +71,7 @@ func (r *RuleSet) UnmarshalJSON(bytes []byte) error {
 		r.Type = C.RuleSetTypeInline
 		v = &r.InlineOptions
 	case C.RuleSetTypeLocal:
-		v = nil
+		v = &r.LocalOptions
 	case C.RuleSetTypeRemote:
 		v = &r.RemoteOptions
 	default:
@@ -84,7 +85,7 @@ func (r *RuleSet) UnmarshalJSON(bytes []byte) error {
 		if r.Format == "" {
 			switch r.Type {
 			case C.RuleSetTypeLocal:
-				r.Format = ruleSetDefaultFormat(r.Path)
+				r.Format = ruleSetDefaultFormat(r.LocalOptions.Path)
 			case C.RuleSetTypeRemote:
 				r.Format = ruleSetDefaultFormat(r.RemoteOptions.URL)
 			}
@@ -98,7 +99,6 @@ func (r *RuleSet) UnmarshalJSON(bytes []byte) error {
 		}
 	} else {
 		r.Format = ""
-		r.Path = ""
 	}
 	return nil
 }
@@ -115,6 +115,10 @@ func ruleSetDefaultFormat(path string) string {
 	default:
 		return ""
 	}
+}
+
+type LocalRuleSet struct {
+	Path string `json:"path,omitempty"`
 }
 
 type RemoteRuleSet struct {
