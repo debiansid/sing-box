80794154bb781d541af6ded4f932e5c1fa153d89
xireiki <i@xireiki.com>
2025-10-08 19:07:40 +0800

    feat: Add temporary disable rule support

--- route/route.go
+++ route/route.go
@@ -505,6 +505,9 @@ func (r *Router) matchRule(
 
 match:
 	for currentRuleIndex, currentRule := range r.rules {
+		if currentRule.Disabled() {
+			continue
+		}
 		metadata.ResetRuleCache()
 		if !currentRule.Match(metadata) {
 			continue
@@ -879,4 +882,9 @@ func isAllIPv6(addresses []netip.Addr) bool {
 		}
 	}
 	return true
+}
+
+func (r *Router) Rule(uuid string) (adapter.Rule, bool) {
+	rule, exists := r.ruleByUUID[uuid]
+	return rule, exists
 }
