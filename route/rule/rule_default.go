b9534b321581f99de8d26c7ff71ddc87043f2dff
xireiki <i@xireiki.com>
2025-10-08 19:07:40 +0800

    Support temporary disable rule

--- route/rule/rule_default.go
+++ route/rule/rule_default.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/sagernet/sing-box/adapter"
+	"github.com/sagernet/sing-box/common/urltest"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
 	"github.com/sagernet/sing/common"
 	E "github.com/sagernet/sing/common/exceptions"
 	"github.com/sagernet/sing/service"
+
+	"github.com/gofrs/uuid/v5"
 )
 
 func NewRule(ctx context.Context, logger log.ContextLogger, options option.Rule, checkOutbound bool) (adapter.Rule, error) {
@@ -57,8 +60,13 @@ func NewDefaultRule(ctx context.Context, logger log.ContextLogger, options optio
 	if err != nil {
 		return nil, E.Cause(err, "action")
 	}
+	id, _ := uuid.NewV4()
 	rule := &DefaultRule{
 		abstractDefaultRule{
+			abstractRule: abstractRule{
+				uuid:    id.String(),
+				history: service.PtrFromContext[urltest.HistoryStorage](ctx),
+			},
 			invert: options.Invert,
 			action: action,
 		},
@@ -320,8 +328,13 @@ func NewLogicalRule(ctx context.Context, logger log.ContextLogger, options optio
 	if err != nil {
 		return nil, E.Cause(err, "action")
 	}
+	id, _ := uuid.NewV4()
 	rule := &LogicalRule{
 		abstractLogicalRule{
+			abstractRule: abstractRule{
+				uuid:    id.String(),
+				history: service.PtrFromContext[urltest.HistoryStorage](ctx),
+			},
 			rules:  make([]adapter.HeadlessRule, len(options.Rules)),
 			invert: options.Invert,
 			action: action,
