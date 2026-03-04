9ba978196e9b1af13500423853fed4821cd3c532
reF1nd <ref1nd@thunder-cloud.cc>
2026-03-04 18:13:00 +0800

    clash-api: Add DNS rules support

--- dns/router.go
+++ dns/router.go
@@ -1380,6 +1380,12 @@ func addressLimitResponseCheck(rule adapter.DNSRule, metadata *adapter.InboundCo
 	}
 }
 
+func (r *Router) Rules() []adapter.DNSRule {
+	r.rulesAccess.RLock()
+	defer r.rulesAccess.RUnlock()
+	return r.rules
+}
+
 func (r *Router) ClearCache() {
 	r.client.ClearCache()
 	if r.platformInterface != nil {
