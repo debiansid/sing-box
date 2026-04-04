091124e9af53f8f4b0c184f7de3ebc6975f6849b
Yuu518 <50163161+Yuu518@users.noreply.github.com>
2026-04-04 13:01:15 +0900

    outbound/group: add fallback group

--- include/registry.go
+++ include/registry.go
@@ -97,6 +97,7 @@ func OutboundRegistry() *outbound.Registry {
 
 	group.RegisterSelector(registry)
 	group.RegisterURLTest(registry)
+	group.RegisterFallback(registry)
 
 	socks.RegisterOutbound(registry)
 	http.RegisterOutbound(registry)
